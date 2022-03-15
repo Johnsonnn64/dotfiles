b09383ed08d814f6bab615a747c33e0210ed7704
Johnsonnn64 <naktacamel2@gmail.com>
2022-03-15 22:17:18 +0800

    dotfiles

--- bin/gitpass.sh
+++ bin/gitpass.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "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" | xclip -selection clipboard
+echo "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" | xclip -selection clipboard
 sleep 0.3
 xdotool key --clearmodifiers ctrl+V
